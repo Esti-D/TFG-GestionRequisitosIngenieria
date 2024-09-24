f2bff1d52c36120793421355fdbe052882367b70
Estibalitz <edr1006@alu.ubu.es>
2024-09-24 18:23:53 +0200

    lectura de pdf issue #8 zube y #3 github

---
+++ extraccion/leer_pdf.py
@@ -0,0 +1,13 @@
+import pdfplumber
+
+def extraer_texto_pdf(ruta_pdf):
+    """Función para extraer el texto de un archivo PDF."""
+    texto_completo = ""
+    try:
+        with pdfplumber.open(ruta_pdf) as pdf:
+            for pagina in pdf.pages:
+                texto_completo += pagina.extract_text() + "\n"
+    except Exception as e:
+        print(f"Error al leer el archivo PDF: {e}")
+    
+    return texto_completo
